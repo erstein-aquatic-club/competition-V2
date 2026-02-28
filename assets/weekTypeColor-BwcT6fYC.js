d8e7ab11ea3566a9e6cc3519b0519829accf1ff6
erstein-aquatic-club <erstein-aquatic-club@users.noreply.github.com>
2026-02-28 14:33:58 +0000

    deploy: c7befc9d331bfbfd655a041af65dab511a77bc3a

---
+++ assets/weekTypeColor-BwcT6fYC.js
@@ -0,0 +1,11 @@
+import{c as o}from"./index-Dm3SzHfx.js";/**
+ * @license lucide-react v0.545.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z",key:"j76jl0"}],["path",{d:"M22 10v6",key:"1lu8f3"}],["path",{d:"M6 12.5V16a6 3 0 0 0 12 0v-3.5",key:"1r8lef"}]],s=o("graduation-cap",n);/**
+ * @license lucide-react v0.545.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],r=o("send",c);function d(t){let a=0;for(let e=0;e<t.length;e++)a=t.charCodeAt(e)+((a<<5)-a);return`hsl(${Math.abs(a)%360}, 70%, 85%)`}function p(t){let a=0;for(let e=0;e<t.length;e++)a=t.charCodeAt(e)+((a<<5)-a);return`hsl(${Math.abs(a)%360}, 60%, 30%)`}export{s as G,r as S,d as a,p as w};
